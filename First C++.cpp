d647de824bc65018299d71691d65e3857821ab63
Tanui Chemutai <51640946+tanuimercychemutai@users.noreply.github.com>
2021-02-21 20:13:50 +0300

    Add files via upload

---
+++ First C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int sum;
+
+	cout << "Enter a number \n";
+	cin >> a;
+
+	cout << "Enter another number \n";
+	cin >> b;
+
+	sum = a + b;
+	cout << "The sum of these numbers is " << sum << endl;
+
+	return 0;
+
+}
+
